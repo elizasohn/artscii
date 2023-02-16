c03720b119b5e9bca70b64e7cddff4741d1abc9f
Peter Wells <unfinishedideas@gmail.com>
2023-02-16 12:41:31 -0800

    initial Player component files created

--- artscii/src/app/App.js
+++ artscii/src/app/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Header from '../components/header/Header';
+import Player from '../components/player/Player';
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
         	<Header />
         	<img className='placeholder-gif' src='https://media.giphy.com/media/fVeAI9dyD5ssIFyOyM/giphy.gif' alt="placeholder-gif"/>
         	<p>Site under construction.</p>
+			<Player/>
         	</div>
     	);
 
---
+++ artscii/src/components/player/Player.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+import './Player.css'
+
+
+
+const Player = () => {
+
+    // const [url, setUrl] = useState(null)
+
+    return(
+        <h1>PLAYER</h1>
+    );
+}
+
+
+
+export default Player
