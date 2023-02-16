c43660f4de78445dd44890898996abb7a586502f
Peter Wells <unfinishedideas@gmail.com>
2023-02-16 14:54:17 -0800

    ported Player code to Main.js

--- artscii/src/components/main/Main.js
+++ artscii/src/components/main/Main.js
@@ -1,10 +1,29 @@
 import './Main.css';
+import Player from '../player/Player';
+import React, { useState } from 'react';
 
 function Main() {
+    // const [url, setUrl] = useState('http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4')
+	// const [search, setSearch] = useState('Pirates')
+	// const [playerMode, setPlayerMode] = useState('mp4')
+
+	const [url, setUrl] = useState('https://media.giphy.com/media/fVeAI9dyD5ssIFyOyM/giphy.gif')
+	const [search, setSearch] = useState('Werk Werk Werk')
+	const [playerMode, setPlayerMode] = useState('image')
+
+    
+	const updatePlayerUrl = (new_url, new_search, new_playerMode) => {
+		setUrl(new_url);
+		setSearch(new_search);
+		setPlayerMode(new_playerMode);
+	}
+
+
     return (
         <div className='main'>
-            <img className='placeholder-gif' src='https://media.giphy.com/media/fVeAI9dyD5ssIFyOyM/giphy.gif' alt="placeholder-gif"/>
-         	<p>Site under construction.</p>
+			<Player url={url} search={search} playerMode={playerMode}/>
+			<p>Site under construction.</p>
+			<button style={{width: '60px', height: '40px'}} onClick={() => updatePlayerUrl('http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4', 'Pirates', 'mp4')}>SWITCH</button>
         </div>
     );
 };
