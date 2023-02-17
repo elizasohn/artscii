1e26ef07b5b6cd96ba3397e2c8d0e1556330ab6b
Peter Wells <unfinishedideas@gmail.com>
2023-02-16 19:03:17 -0800

    added basic URL checking, fixed classname error in display components

--- artscii/src/components/player/Player.js
+++ artscii/src/components/player/Player.js
@@ -3,27 +3,28 @@ import './Player.css'
 
 const Player = (props) => {
     try {
+        // Stretch: Parse URL for mode selection instead of passing in prop? TBD
         if (!props.playerMode) {
             throw new Error('Player Error: No mode sent!')
         }
         if (!props.url) {
             throw new Error('Player Error: No URL sent!')
         }
-        // TODO: check if URL is bad, parse URL for mode selection instead of passing in prop 
-        else {
-            switch(props.playerMode)
-            {
-                case 'image':
-                    return(
-                        <ImageDisplay url={props.url} search={props.search}/>
-                    )
-                case 'mp4':
-                    return(
-                        <Mp4Display url={props.url} search={props.search}/>
-                    )
-                default:
-                    throw new Error('Player Error: Bad mode!');
-            }
+        // Throws an exception if string is not in URL format
+        let processed_url = new URL(props.url);
+
+        switch(props.playerMode)
+        {
+            case 'image':
+                return(
+                    <ImageDisplay url={processed_url.href} search={props.search}/>
+                )
+            case 'mp4':
+                return(
+                    <Mp4Display url={processed_url.href} search={props.search}/>
+                )
+            default:
+                throw new Error('Player Error: Bad mode!');
         }
     }
     catch (err) {
@@ -32,20 +33,21 @@ const Player = (props) => {
     }
 }
 
+// Individual display components for different media
 const ImageDisplay = (props) => {
     return(
-        <div class='player-window'>
-            <h2 class='title-text'>{props.search}</h2>
-            <img src={props.url} alt={props.search} class='image-display'/>
+        <div className='player-window'>
+            <h2 className='title-text'>{props.search}</h2>
+            <img src={props.url} alt={props.search} className='image-display'/>
         </div>
     )
 }
 
 const Mp4Display = (props) => {
     return(
-        <div class='player-window'>
-            <h2 class='title-text'>{props.search}</h2>
-            <video class='video-display' controls>
+        <div className='player-window'>
+            <h2 className='title-text'>{props.search}</h2>
+            <video className='video-display' controls>
                 <source src={props.url} type="video/mp4"/>
             </video>
         </div>
@@ -54,11 +56,10 @@ const Mp4Display = (props) => {
 
 const ErrorStateDisplay = () => {
     return(
-        <div class='player-window'>
+        <div className='player-window'>
             <h1>Something went wrong! Sorry about that</h1>
         </div>
     )
 }
-
 
 export default Player
