bdcc9f0d05892d6131f9a72ee47c567abb330584
Stu <78074492+srimel@users.noreply.github.com>
2023-03-01 15:25:32 -0800

    Removed about title, cursive font, made link color lighter

--- artscii/src/components/about/About.js
+++ artscii/src/components/about/About.js
@@ -4,16 +4,13 @@ import Title from '../title/Title'
 function About() {
     return (
         <div className="about-container">
-            <div className="about-header">
-                <h2>About Us</h2>
-            </div>
             <div className="about-content">
                 <p>
                     Introducing Asciiart, our web app utilizes Dezgo's text-to-image API to generate images, which can then be converted into ASCII art.
                 </p>
                 <p>
                     Our responsive and user-friendly interface allows for easy input of text that will run through a stable diffusion model to generate a image.  
-                    <a href='https://en.wikipedia.org/wiki/Stable_Diffusion'>Click here</a> to learn more about stable diffusion!
+                    <a id='info-link' href='https://en.wikipedia.org/wiki/Stable_Diffusion'>Click here</a> to learn more about stable diffusion!
                 </p>
                 <p>
                     Give Asciiart a try and watch as your words come to life in the most unique and visually appealing way possible!
