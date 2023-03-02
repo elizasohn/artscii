abcdd56fc7510ad2d252735ac14f959d428dd4da
Eliza Sohn <eliza.sohn@gmail.com>
2023-03-01 23:51:12 -0800

    Update stableDiffusionService.js

--- artscii/src/services/stableDiffusionService.js
+++ artscii/src/services/stableDiffusionService.js
@@ -1,8 +1,9 @@
 import axios from "axios";
+import home_gif from '../assets/home.gif'
 let Buffer = require('buffer').Buffer
 
 const STABLE_DIFFUSION_API_KEY = process.env.REACT_APP_STABLE_DIFFUSION_API_KEY;
-const DEFAULT_IMAGE_TO_SHOW = `https://media.giphy.com/media/fVeAI9dyD5ssIFyOyM/giphy.gif`;
+const DEFAULT_IMAGE_TO_SHOW = home_gif;
 const DEZGO_HOST_URL = `https://dezgo.p.rapidapi.com/text2image`;
 const DEFAULT_SEARCH_TEXT = `an astronaut riding a horse, digital art, epic lighting, highly-detailed masterpiece trending HQ`;
 
@@ -23,7 +24,6 @@ export async function getStableDiffusionImageBySearchText(searchText = "",
     encodedParams.append("prompt", searchText);
     encodedParams.append("model", model);
     encodedParams.append("guidance", guidance);
-
     // getting the response from Dezgo API
     const imageBase64 = await callStableDiffusionWithPrompt(encodedParams);
     if (imageBase64 === undefined) {
@@ -33,7 +33,6 @@ export async function getStableDiffusionImageBySearchText(searchText = "",
     var image = `data:image/png;base64, ${imageBase64}`;
     return image;
 }
-
 // function to call the Stable Diffusion API
 async function callStableDiffusionWithPrompt(encodedParams) {
     const options = {
