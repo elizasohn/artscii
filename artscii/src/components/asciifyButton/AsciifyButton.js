ed67cac749a9030a5581e48d154041d9cd2c574e
Eliza Sohn <eliza.sohn@gmail.com>
2023-03-21 15:07:25 -0700

    Update AsciifyButton.js

--- artscii/src/components/asciifyButton/AsciifyButton.js
+++ artscii/src/components/asciifyButton/AsciifyButton.js
@@ -3,7 +3,7 @@ import './AsciifyButton.css';
 
 const AsciifyButton = (props) => {
     return (
-        props.searchActive && (
+        props.searchActive && props.displayMode === 'image' && (
             <div className='ascii-button-container'>
                 <button className='ascii-button' onClick={props.asciify}>asciify</button>
             </div>
