c4226131959a5ec2358c7dca844bbb932698e409
Eliza Sohn <eliza.sohn@gmail.com>
2023-03-21 12:27:31 -0700

    Update services.js

--- artscii/src/services/services.js
+++ artscii/src/services/services.js
@@ -2,6 +2,9 @@ import convertToGrayScales from "./convertToGrayScales";
 import drawAscii from "./drawAscii";
 import { getStableDiffusionImageBySearchText } from "./stableDiffusionService";
 import { getGiphyImageBySearchText } from "./giphyService";
+import loadVideoToCanvas from "./loadVideoToCanvas";
+import convertGifToMp4 from "./convertGifToMp4";
+import convertCanvasVideoToAscii from "./convertCanvasVideoToAscii";
 
 
-export { convertToGrayScales, drawAscii, getStableDiffusionImageBySearchText, getGiphyImageBySearchText };
+export { convertToGrayScales, drawAscii, getStableDiffusionImageBySearchText, getGiphyImageBySearchText, loadVideoToCanvas, convertGifToMp4, convertCanvasVideoToAscii };
