900393182b7640aebfbd6e484cbf5c4f419425f0
Eliza Sohn <eliza.sohn@gmail.com>
2023-03-21 12:16:10 -0700

    Update components.js

--- artscii/src/components/components.js
+++ artscii/src/components/components.js
@@ -1,10 +1,12 @@
 import About from "./about/About";
 import AsciifyButton from "./asciifyButton/AsciifyButton";
+import AsciifyGifButton from "./asciifyGifButton/AsciifyGifButton";
 import DisplayManager from "./displayManager/DisplayManager";
 import Input from "./input/Input";
 import Navbar from "./navbar/Navbar";
 import SearchTextTitle from "./searchTextTitle/SearchTextTitle";
 import Title from "./title/Title";
+import DownloadButton from "./downloadButton/DownloadButton";
 
 
-export { About, AsciifyButton, DisplayManager, Input, Navbar, SearchTextTitle, Title };
+export { About, AsciifyButton, AsciifyGifButton, DisplayManager, Input, Navbar, SearchTextTitle, Title, DownloadButton };
